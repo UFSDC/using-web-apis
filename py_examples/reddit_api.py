054cc78b0edd60f3c99150c1a2687d05be239519
Colin Chan <colinchan@lumeh.org>
2015-02-25 18:18:13 -0500

    Clean up the Python code

--- py_examples/reddit_api.py
+++ py_examples/reddit_api.py
@@ -1,26 +1,24 @@
-import sys, requests, json
+import json
+import requests
+import sys
 
-#shorten calling command line arguments
-args = sys.argv
+# only access the api when we're given a commandline arg
+if len(sys.argv) == 2:
+    # the url we're requesting from with the commandline arg put in
+    url = "http://www.reddit.com/user/%s/comments/.json" % sys.argv[1]
 
-#only access the api when we're given a commandline arg
-if len(sys.argv)==2:
-    #the url we're requesting from with the commandline arg put in
-    url = "http://www.reddit.com/user/"+str(args[1])+"/comments/.json"
-
-    #specify your user-agent to get more leniency with the api
+    # specify your user-agent to get more leniency with the api
     headers = {
-	'User-Agent':'Mememe',
-	'From':'andrewjvora@gmail.com'
+        'User-Agent': 'Mememe',
+        'From': 'andrewjvora@gmail.com',
     }
-    
-    #use the requests library to send a get request
+
+    # use the requests library to send a get request
     r = requests.get(url, headers=headers)
-    
-    #print the response text
+
+    # print the response text
     parsed = json.loads(r.text)
     print json.dumps(parsed, indent=4, sort_keys=True)
 else:
-   #tell them their mistake
+    # tell them their mistake
     print "Soz. Bad number of args."
-
